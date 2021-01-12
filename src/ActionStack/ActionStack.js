a65effcfedf917b7cd7e667d76da261c52fc561b
gergooo <39014407+gergooo@users.noreply.github.com>
2021-01-12 11:32:35 +0100

    unit tests are added for ActionStack's logic

--- src/ActionStack/ActionStack.js
+++ src/ActionStack/ActionStack.js
@@ -1,118 +1,120 @@
-import _ from 'lodash';
 import { useState } from 'react';
 
 export function useActionStack(initialState) {
-  const [history, setHistory] = useState(createInitialHistory(initialState));
-
-  const numberOfAvailableUndos = history.historyIndex;
-  const numberOfAvailableRedos = Math.max(
-    history.array.length - 1 - history.historyIndex,
-    0
+  const [history, setHistory] = useState(
+    Logic.createInitialHistory(initialState)
   );
 
   return {
-    state: history.array[history.historyIndex],
-    onAction: (newState) => setHistory(onAction(newState)),
-    onUndo: (steps) => setHistory(onUndo(steps)),
-    onRedo: (steps) => setHistory(onRedo(steps)),
-    isUndoAvailable: numberOfAvailableUndos > 0,
-    isRedoAvailable: numberOfAvailableRedos > 0,
-    numberOfAvailableUndos,
-    numberOfAvailableRedos,
+    state: Logic.getCurrentState(history),
+    onAction: (newState) => setHistory(Logic.onAction(newState)),
+    onUndo: (steps) => setHistory(Logic.onUndo(steps)),
+    onRedo: (steps) => setHistory(Logic.onRedo(steps)),
+    numberOfAvailableUndos: Logic.getNumberOfAvailableUndos(history),
+    numberOfAvailableRedos: Logic.getNumberOfAvailableRedos(history),
   };
 }
 
-const createInitialHistory = (initialState) => ({
-  array: [initialState],
-  historyIndex: 0,
-});
+export class Logic {
+  static createInitialHistory = (initialState) => ({
+    array: [initialState],
+    historyIndex: 0,
+  });
 
-const onAction = (newState) => (history) => {
-  const previousState = history.array[history.historyIndex];
-  const diffForUndo = getDifferences(previousState, newState);
+  static getCurrentState = (history) => history.array[history.historyIndex];
 
-  if (Object.keys(diffForUndo).length > 0) {
-    return {
-      array: [
-        ...history.array.slice(0, history.historyIndex),
-        diffForUndo,
-        newState,
-      ],
-      historyIndex: history.historyIndex + 1,
-    };
-  } else {
-    return history;
-  }
-};
+  static getNumberOfAvailableUndos = (history) => history.historyIndex;
 
-const onUndo = (steps = 1) => (history) => {
-  if (steps <= history.historyIndex) {
-    let historyDraft = { ...history };
+  static getNumberOfAvailableRedos = (history) =>
+    Math.max(history.array.length - 1 - history.historyIndex, 0);
 
-    for (let i = 0; i < steps; i++) {
-      const currentState = historyDraft.array[historyDraft.historyIndex];
-      const previousState = {
-        ...currentState,
-        ...historyDraft.array[historyDraft.historyIndex - 1],
-      };
-      const diffForRedo = getDifferences(currentState, previousState);
+  static onAction = (newState) => (history) => {
+    const previousState = history.array[history.historyIndex];
+    const diffForUndo = Logic.getDifferences(previousState, newState);
 
-      historyDraft = {
+    if (Object.keys(diffForUndo).length > 0) {
+      return {
         array: [
-          ...historyDraft.array.slice(0, historyDraft.historyIndex - 1),
-          previousState,
-          diffForRedo,
-          ...historyDraft.array.slice(historyDraft.historyIndex + 1),
-        ],
-        historyIndex: historyDraft.historyIndex - 1,
-      };
-    }
-
-    return historyDraft;
-  } else {
-    return history;
-  }
-};
-
-const onRedo = (steps = 1) => (history) => {
-  if (steps <= history.array.length - 1 - history.historyIndex) {
-    let historyDraft = { ...history };
-
-    for (let i = 0; i < steps; i++) {
-      const nextState = {
-        ...historyDraft.array[historyDraft.historyIndex],
-        ...historyDraft.array[historyDraft.historyIndex + 1],
-      };
-      const diffForUndo = getDifferences(
-        historyDraft.array[historyDraft.historyIndex],
-        nextState
-      );
-
-      historyDraft = {
-        array: [
-          ...historyDraft.array.slice(0, historyDraft.historyIndex),
+          ...history.array.slice(0, history.historyIndex),
           diffForUndo,
-          nextState,
-          ...historyDraft.array.slice(historyDraft.historyIndex + 2),
+          newState,
         ],
-        historyIndex: historyDraft.historyIndex + 1,
+        historyIndex: history.historyIndex + 1,
       };
+    } else {
+      return history;
     }
-    return historyDraft;
-  } else {
-    return history;
-  }
-};
+  };
 
-const getDifferences = (object, referenceObject) => {
-  const differences = {};
+  static onUndo = (steps = 1) => (history) => {
+    if (steps <= history.historyIndex) {
+      let historyDraft = { ...history };
 
-  for (const key in referenceObject) {
-    if (Object.hasOwnProperty.call(referenceObject, key)) {
-      if (referenceObject[key] !== object[key]) {
-        differences[key] = object[key];
+      for (let i = 0; i < steps; i++) {
+        const currentState = historyDraft.array[historyDraft.historyIndex];
+        const previousState = {
+          ...currentState,
+          ...historyDraft.array[historyDraft.historyIndex - 1],
+        };
+        const diffForRedo = Logic.getDifferences(currentState, previousState);
+
+        historyDraft = {
+          array: [
+            ...historyDraft.array.slice(0, historyDraft.historyIndex - 1),
+            previousState,
+            diffForRedo,
+            ...historyDraft.array.slice(historyDraft.historyIndex + 1),
+          ],
+          historyIndex: historyDraft.historyIndex - 1,
+        };
+      }
+
+      return historyDraft;
+    } else {
+      return history;
+    }
+  };
+
+  static onRedo = (steps = 1) => (history) => {
+    if (steps <= history.array.length - 1 - history.historyIndex) {
+      let historyDraft = { ...history };
+      for (let i = 0; i < steps; i++) {
+        const nextState = {
+          ...historyDraft.array[historyDraft.historyIndex],
+          ...historyDraft.array[historyDraft.historyIndex + 1],
+        };
+        const diffForUndo = Logic.getDifferences(
+          historyDraft.array[historyDraft.historyIndex],
+          nextState
+        );
+
+        historyDraft = {
+          array: [
+            ...historyDraft.array.slice(0, historyDraft.historyIndex),
+            diffForUndo,
+            nextState,
+            ...historyDraft.array.slice(historyDraft.historyIndex + 2),
+          ],
+          historyIndex: historyDraft.historyIndex + 1,
+        };
+      }
+
+      return historyDraft;
+    } else {
+      return history;
+    }
+  };
+
+  static getDifferences = (object, referenceObject) => {
+    const differences = {};
+
+    for (const key in referenceObject) {
+      if (Object.hasOwnProperty.call(referenceObject, key)) {
+        if (referenceObject[key] !== object[key]) {
+          differences[key] = object[key];
+        }
       }
     }
-  }
-  return differences;
-};
+    return differences;
+  };
+}
---
+++ src/ActionStack/ActionStack.unit.test.js
@@ -0,0 +1,201 @@
+import { Logic } from './ActionStack';
+
+describe('ActionStackLogic unit tests', () => {
+  // Here are some reused objects
+  let initialState, initialHistory, historyWith4Undos, historyWith4Redos;
+
+  beforeEach(() => {
+    initialState = { a: 'a', b: 13, c: true };
+
+    initialHistory = Logic.createInitialHistory(initialState);
+
+    let temp1 = initialHistory;
+    let temp2 = Logic.onAction({ a: 'blab', b: 13, c: true })(temp1);
+    let temp3 = Logic.onAction({ a: 'blab', b: 666, c: true })(temp2);
+    let temp4 = Logic.onAction({ a: 'blab', b: -3, c: false })(temp3);
+    historyWith4Undos = Logic.onAction({ a: 'blab', b: -3, c: true })(temp4);
+
+    historyWith4Redos = Logic.onUndo(4)(historyWith4Undos);
+  });
+
+  test('createInitialHistory initializes complex history state', () => {
+    const initialState = { a: 'a', b: 13, c: true };
+
+    const initialHistory = Logic.createInitialHistory(initialState);
+
+    expect(initialHistory).toStrictEqual({
+      array: [{ a: 'a', b: 13, c: true }],
+      historyIndex: 0,
+    });
+  });
+
+  describe('actions', () => {
+    describe('onAction', () => {
+      describe('1st action', () => {
+        let history1stStep;
+
+        beforeAll(() => {
+          history1stStep = Logic.onAction({ a: 'a', b: 14, c: true })(
+            initialHistory
+          );
+        });
+
+        test("new history's length is 2", () => {
+          expect(history1stStep.array.length).toBe(2);
+        });
+
+        test("onAction is pure - doesn't mutate input history", () => {
+          expect(initialHistory).toStrictEqual({
+            array: [{ a: 'a', b: 13, c: true }],
+            historyIndex: 0,
+          });
+        });
+
+        test('history array contains diff for undo instead of full state', () => {
+          expect(history1stStep).toStrictEqual({
+            array: [{ b: 13 }, { a: 'a', b: 14, c: true }],
+            historyIndex: 1,
+          });
+        });
+      });
+
+      test('multiple actions - undo diff is stored', () => {
+        expect(historyWith4Undos).toStrictEqual({
+          array: [
+            { a: 'a' },
+            { b: 13 },
+            { b: 666, c: true },
+            { c: false },
+            { a: 'blab', b: -3, c: true },
+          ],
+          historyIndex: 4,
+        });
+      });
+
+      test('nothing is changed, nothing is stored', () => {
+        const history = Logic.onAction(initialState)(initialHistory);
+
+        expect(history).toStrictEqual(initialHistory);
+      });
+
+      // not handled: if the new state has different keys as the previous one
+    });
+
+    describe('onUndo/onRedo', () => {
+      describe('onUndo', () => {
+        test('redo diff is calculated, historyIndex points to current state', () => {
+          let undoneHistory = Logic.onUndo()(historyWith4Undos);
+
+          expect(undoneHistory).toStrictEqual({
+            array: [
+              { a: 'a' },
+              { b: 13 },
+              { b: 666, c: true },
+              { a: 'blab', b: -3, c: false },
+              { c: true },
+            ],
+            historyIndex: 3,
+          });
+        });
+
+        test('multiple-step undo', () => {
+          let totallyUndoneHistory = Logic.onUndo(4)(historyWith4Undos);
+
+          expect(totallyUndoneHistory).toStrictEqual({
+            array: [
+              { a: 'a', b: 13, c: true },
+              { a: 'blab' },
+              { b: 666 },
+              { b: -3, c: false },
+              { c: true },
+            ],
+            historyIndex: 0,
+          });
+        });
+
+        test('does nothing when there is nothing to undo', () => {
+          const sameHistory = Logic.onUndo()(historyWith4Redos);
+          expect(sameHistory).toStrictEqual(historyWith4Redos);
+        });
+
+        test('does nothing when undo step is too large', () => {
+          const sameHistory = Logic.onUndo(5)(historyWith4Undos);
+          expect(sameHistory).toStrictEqual(historyWith4Undos);
+        });
+      });
+
+      describe('onRedo', () => {
+        test('undo diff is calculated, historyIndex points to current state', () => {
+          let redoneHistory = Logic.onRedo()(historyWith4Redos);
+
+          expect(redoneHistory).toStrictEqual({
+            array: [
+              { a: 'a' },
+              { a: 'blab', b: 13, c: true },
+              { b: 666 },
+              { b: -3, c: false },
+              { c: true },
+            ],
+            historyIndex: 1,
+          });
+        });
+
+        test('multiple-step redo', () => {
+          let totallyRedoneHistory = Logic.onRedo(4)(historyWith4Redos);
+
+          expect(totallyRedoneHistory).toStrictEqual({
+            array: [
+              { a: 'a' },
+              { b: 13 },
+              { b: 666, c: true },
+              { c: false },
+              { a: 'blab', b: -3, c: true },
+            ],
+            historyIndex: 4,
+          });
+        });
+
+        test('does nothing when there is nothing to redo', () => {
+          const sameHistory = Logic.onRedo()(historyWith4Undos);
+          expect(sameHistory).toStrictEqual(historyWith4Undos);
+        });
+
+        test('does nothing when undo step is too large', () => {
+          const sameHistory = Logic.onUndo(5)(historyWith4Redos);
+          expect(sameHistory).toStrictEqual(historyWith4Redos);
+        });
+      });
+    });
+  });
+
+  describe('helpers', () => {
+    test('getCurrentState', () => {
+      expect(Logic.getCurrentState(historyWith4Undos)).toStrictEqual(
+        historyWith4Undos.array[4]
+      );
+
+      const undoneHistory = Logic.onUndo()(historyWith4Undos);
+      expect(Logic.getCurrentState(undoneHistory)).toStrictEqual(
+        undoneHistory.array[3]
+      );
+    });
+
+    test('getNumberOfAvailableUndos', () => {
+      expect(Logic.getNumberOfAvailableUndos(historyWith4Undos)).toBe(4);
+
+      const historyWith3Undos = Logic.onUndo()(historyWith4Undos);
+      expect(Logic.getNumberOfAvailableUndos(historyWith3Undos)).toBe(3);
+
+      expect(Logic.getNumberOfAvailableUndos(historyWith4Redos)).toBe(0);
+    });
+
+    test('getNumberOfAvailableRedos', () => {
+      expect(Logic.getNumberOfAvailableRedos(historyWith4Redos)).toBe(4);
+
+      const historyWith3Redos = Logic.onRedo(2)(historyWith4Redos);
+      expect(Logic.getNumberOfAvailableRedos(historyWith3Redos)).toBe(2);
+
+      expect(Logic.getNumberOfAvailableRedos(historyWith4Undos)).toBe(0);
+    });
+  });
+});
